22b0838436443e6283a991eb17119ea490507e37
amod1811 <ammch101@gmail.com>
2022-06-24 00:58:49 +0530

    Added program to part time  employee wage calculate

--- EmpolyeeWage.java
+++ EmpolyeeWage.java
@@ -1,17 +1,21 @@
 package bl.com.employeewageprogram;
-
+/*
+ * Author- Amod
+ * Part time Employee&Wage Calculate
+ */
 public class EmpolyeeWage {
 
 	public static void main(String[] args) {
 		//variables
 		int IS_FULL_TIME =1;
+		int IS_PART_TIME = 2;
 		int  empHrs=8;
 		int Wsa =0;
 		int EMP_RATE_PER_HOUR = 20;
 		int empWage = 20;
 		
 		//use random method check employee is present or not
-		double empCheck = Math.floor(Math.random()* 10)%2;
+		double empCheck = Math.floor(Math.random()* 10)%3;
 		if (empCheck == IS_FULL_TIME)
 			System.out.println("Employee is present");
 		else
@@ -20,6 +24,10 @@ public class EmpolyeeWage {
 		//calculate employee wage 
 		if (empCheck == IS_FULL_TIME)
 			empHrs = 12;
+		else if (empCheck == IS_PART_TIME)
+			empHrs = 8;
+		else
+			empHrs = 0;
 		empWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Emp Wage : " + empWage);
 				
