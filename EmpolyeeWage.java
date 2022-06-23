729fe0377671a16bf3f752a529af72bad55f4c80
amod1811 <ammch101@gmail.com>
2022-06-24 00:40:02 +0530

    Added program Employee is present or Not

---
+++ EmpolyeeWage.java
@@ -0,0 +1,17 @@
+package bl.com.employeewageprogram;
+
+public class EmpolyeeWage {
+
+	public static void main(String[] args) {
+		int IS_FULL_TIME =1;
+		int  empHrs=8;
+		int Wsa =0;
+		double empCheck = Math.floor(Math.random()* 10)%2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is not Absent");
+
+	}
+
+}
